ee3d2306d6278f3bd6ecec39dd62d9be0a3b7aa9
Brandon Urbina <burbina95@yahoo.com>
2020-09-07 18:33:38 -0400

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Brandon Urbina on 9/7/20.
+//  Copyright © 2020 Brandon Urbina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
