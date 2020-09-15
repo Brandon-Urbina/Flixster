79bb2957cd807048bca6e3bbd55e52e7cfb57082
Brandon Urbina <burbina95@yahoo.com>
2020-09-14 23:27:27 -0400

    Flix Part 2 added

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Brandon Urbina on 9/14/20.
+//  Copyright © 2020 Brandon Urbina. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
